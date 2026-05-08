5d2989588b5ccbd4964b09d12a4b4347285d2c14
FatahChan <ahmadfathallah89@gmail.com>
2026-05-09 00:43:50 +0300

    Update RootDocument component to suppress hydration warnings in HTML element for improved rendering consistency.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -47,7 +47,7 @@ function RootComponent() {
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <HeadContent />
       </head>
